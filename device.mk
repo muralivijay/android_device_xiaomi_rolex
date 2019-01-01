0ac22a35dcb69a3fe579634f0e57d814301f3fe6
muralivijay <muralidharan9845@gmail.com>
2019-01-01 13:04:24 +0000

    Revert "rolex: Add perf configs"
    This reverts commit acd43029f1ff1eff4531fd75c824905abd96b41b.

--- device.mk
+++ device.mk
@@ -292,11 +292,8 @@ PRODUCT_PACKAGES += \
     libOmxVenc \
     libstagefrighthw
 
-# Perf configuration
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml \
-    $(LOCAL_PATH)/configs/perf-profile0.conf:$(TARGET_COPY_OUT_VENDOR)/etc/perf/perf-profile0.conf \
-    $(LOCAL_PATH)/configs/perfboostsconfig.xml:$(TARGET_COPY_OUT_VENDOR)/etc/perf/perfboostsconfig.xml 
+    $(LOCAL_PATH)/configs/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
 
 # Properties
 -include device/xiaomi/rolex/vendor_prop.mk
