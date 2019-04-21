b7d1576bf101b3e69c2d2baa257351b7607442e2
muralivijay <muralidharan9845@gmail.com>
2019-04-21 16:49:06 +0000

    rolex: Update cryptfs_hw path

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -114,7 +114,7 @@ BOARD_USES_QCNE := true
 
 # Crypto
 TARGET_HW_DISK_ENCRYPTION := true
-TARGET_CRYPTFS_HW_PATH ?= vendor/qcom/opensource/cryptfs_hw
+TARGET_CRYPTFS_HW_PATH ?= vendor/qcom/opensource/commonsys/cryptfs_hw
 
 # Dexpreopt
 ifeq ($(HOST_OS),linux)
